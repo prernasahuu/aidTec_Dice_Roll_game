6be3ad9c779127b1f997ce1f7185cd841aa1aee6
Prerna Sahu <123632781+prernasahuu@users.noreply.github.com>
2023-05-29 07:30:54 +0530

    Dice Roll game.py

--- Dice Roll game.py
+++ Dice Roll game.py
@@ -1,24 +1,23 @@
 import random
-def roll_dice_game():
-  print("WELCOME TO THE DICE ROLLING GAME!")
 
-  while True:
-    one_dice=random.randint(1,6) 
-    two_dice= random.randint(1,6)
-    guess = int(input("Enter your guess (2 to 12): "))
-    sum= one_dice+two_dice
-    print(f"DICE 1: {one_dice}")
-    print(f"DICE 2: {two_dice}")
-    print(f"SUM OF DICE: {sum}")
-    
-    if guess == sum :
-      print(f"CONGRATULATIONS!!")
-      print("Bravo!! You're the WINNER.")
-      break
-    else:
-      print(f"Game Over! WRONG GUESS")
-      print("PLAY AGAIN?")
-      quit = input("Do you want to play again? (yes/quit):")
-      if quit.lower()!="yes":
-        break
-roll_dice_game()
+def roll_dice(num_dice):
+    dice_rolls = []
+    for _ in range(num_dice):
+        dice_roll = random.randint(1, 6)
+        dice_rolls.append(dice_roll)
+    return dice_rolls
+
+def dice_game():
+    print("Welcome to the Dice Game!")
+    print("Roll the dice and see the outcomes.")
+
+    while True:
+        num_dice = int(input("Enter the number of dice to roll: "))
+        dice_rolls = roll_dice(num_dice)
+        
+        print("Dice Rolls:", dice_rolls)
+        
+        play_again = input("Do you want to play again? (yes/quit): ")
+        if play_again.lower() != "yes":
+            break
+dice_game()
